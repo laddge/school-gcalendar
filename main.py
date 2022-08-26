0788efa62d01a6c5b443a56ee671d378db191097
Laddge <dev.laddge@gmail.com>
2022-08-26 12:42:30 +0000

    [feat] カレンダーの内容を取得

--- main.py
+++ main.py
@@ -5,12 +5,14 @@ import google.auth
 
 def main():
     with open("./config.toml") as f:
-        config = toml.loads(f.read())
+        config = toml.load(f)
     creds = google.auth.load_credentials_from_file(
         "./credentials.json",
         ["https://www.googleapis.com/auth/calendar"],
     )[0]
     service = googleapiclient.discovery.build("calendar", "v3", credentials=creds)
+    events = service.events().list(calendarId=config["calendarId"]).execute()
+    print(events)
 
 
 if __name__ == "__main__":
