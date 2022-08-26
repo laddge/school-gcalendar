03579b1d2fcbafee3e97c9da7d68da2521079693
Laddge <dev.laddge@gmail.com>
2022-08-26 13:53:25 +0000

    [feat] コード完成

--- main.py
+++ main.py
@@ -11,8 +11,17 @@ def main():
         ["https://www.googleapis.com/auth/calendar"],
     )[0]
     service = googleapiclient.discovery.build("calendar", "v3", credentials=creds)
-    events = service.events().list(calendarId=config["calendarId"]).execute()
-    print(events)
+    events = service.events().list(calendarId=config["calendarId"]).execute().get("items", [])
+    for date, table in config["dates"].items():
+        for event in events:
+            if date in event["start"]["dateTime"]:
+                service.events().delete(calendarId=config["calendarId"], eventId=event["id"]).execute()
+        if type(table) == str:
+            table = config["presets"][table]
+        for i, event in enumerate(table):
+            event["start"] = {"dateTime": "{}T{}:00".format(date, config["periods"][i][0]), "timeZone": config["tz"]}
+            event["end"] = {"dateTime": "{}T{}:00".format(date, config["periods"][i][1]), "timeZone": config["tz"]}
+            service.events().insert(calendarId=config["calendarId"], body=event).execute()
 
 
 if __name__ == "__main__":
