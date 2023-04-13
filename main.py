6a8f1e201ccc0dd0f47650e129d46ac8b7264a17
Laddge <dev.laddge@gmail.com>
2023-04-13 01:22:19 +0000

    update

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import toml
 import googleapiclient.discovery
 import google.auth
+import tqdm
 
 
 def main():
@@ -13,15 +14,19 @@ def main():
     service = googleapiclient.discovery.build("calendar", "v3", credentials=creds)
     events = service.events().list(calendarId=config["calendarId"], maxResults=9999).execute().get("items", [])
     for date, table in config["dates"].items():
-        for event in events:
+        print(f"\n\033[32;1m[{date}]\033[m")
+        print(f"Clearing...")
+        for event in tqdm.tqdm(events):
             if date in event["start"]["dateTime"]:
                 service.events().delete(calendarId=config["calendarId"], eventId=event["id"]).execute()
         if type(table) == str:
             table = config["presets"][table]
-        for i, event in enumerate(table):
-            event["start"] = {"dateTime": "{}T{}:00".format(date, config["periods"][i][0]), "timeZone": config["tz"]}
-            event["end"] = {"dateTime": "{}T{}:00".format(date, config["periods"][i][1]), "timeZone": config["tz"]}
-            service.events().insert(calendarId=config["calendarId"], body=event).execute()
+        print(f"Writing...")
+        for i, event in enumerate(tqdm.tqdm(table)):
+            if "summary" in event:
+                event["start"] = {"dateTime": "{}T{}:00".format(date, config["periods"][i][0]), "timeZone": config["tz"]}
+                event["end"] = {"dateTime": "{}T{}:00".format(date, config["periods"][i][1]), "timeZone": config["tz"]}
+                service.events().insert(calendarId=config["calendarId"], body=event).execute()
 
 
 if __name__ == "__main__":
